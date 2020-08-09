620f8f67b2e83e9a275db803acea919d066d9136
sariug <ugurcansari93@gmail.com>
2020-08-09 10:42:22 +0200

    Quad is also included

---
+++ src/Quad2DShaderProgram.cpp
@@ -0,0 +1,85 @@
+#include "Quad2DShaderProgram.h"
+#include <Corrade/Containers/Reference.h>
+#include <Corrade/Utility/Resource.h>
+#include <Magnum/GL/Version.h>
+#include <Magnum/GL/Context.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Math/Matrix4.h>
+namespace Magnum
+{
+    namespace Examples
+    {
+
+        Quad2DShaderProgram::Quad2DShaderProgram()
+        {
+            MAGNUM_ASSERT_GL_VERSION_SUPPORTED(GL::Version::GL400);
+
+            /* Load and compile shaders from compiled-in resource */
+            Utility::Resource rs("Shaders-glsl");
+
+            GL::Shader vs{GL::Version::GL400, GL::Shader::Type::Vertex};
+            GL::Shader tcs{GL::Version::GL400, GL::Shader::Type::TessellationControl};
+            GL::Shader tes{GL::Version::GL400, GL::Shader::Type::TessellationEvaluation};
+            GL::Shader gs{GL::Version::GL400, GL::Shader::Type::Geometry};
+            GL::Shader fs{GL::Version::GL400, GL::Shader::Type::Fragment};
+
+            vs.addSource(rs.get("ForAll.vs"));
+            tcs.addSource(rs.get("Quad2D.tcs"));
+            tes.addSource(rs.get("Quad2D.tes"));
+            gs.addSource(rs.get("Quad2D.gs"));
+            fs.addSource(rs.get("Quad2D.fs"));
+
+            CORRADE_INTERNAL_ASSERT_OUTPUT(GL::Shader::compile({vs, tcs, tes, gs, fs}));
+
+            attachShaders({vs, tcs, tes, gs, fs});
+
+            CORRADE_INTERNAL_ASSERT_OUTPUT(link());
+
+            // Retrieve uniforms
+            m_lineColor = uniformLocation("lineColor");
+            m_lineWidth = uniformLocation("lineWidth");
+            m_quadColor = uniformLocation("quadColor");
+            m_viewportMatrix = uniformLocation("ViewportMatrix");
+            m_outer = uniformLocation("Outer");
+            m_inner = uniformLocation("Inner");
+
+            m_transformationProjectionMatrix = uniformLocation("MVP");
+        } // namespace Examples
+        Quad2DShaderProgram &Quad2DShaderProgram::setLineWidth(const float number)
+        {
+            setUniform(m_lineWidth, number);
+            return *this;
+        }
+        Quad2DShaderProgram &Quad2DShaderProgram::setNumberOfOuter(const int number)
+        {
+            setUniform(m_outer, number);
+            return *this;
+        }
+        Quad2DShaderProgram &Quad2DShaderProgram::setNumberOfInner(const int number)
+        {
+            setUniform(m_inner, number);
+            return *this;
+        }
+        Quad2DShaderProgram &Quad2DShaderProgram::setLineColor(const Color3& color)
+        {
+            setUniform(m_lineColor, color);
+            return *this;
+        }
+            Quad2DShaderProgram &Quad2DShaderProgram::setQuadColor(const Color3& color)
+        {
+            setUniform(m_quadColor, color);
+            return *this;
+        }
+        Quad2DShaderProgram &Quad2DShaderProgram::setViewportMatrix(const Matrix4& matrix)
+        {
+            setUniform(m_viewportMatrix, matrix);
+            return *this;
+        }
+        Quad2DShaderProgram &Quad2DShaderProgram::setTransformationProjectionMatrix(const Matrix4& matrix)
+        {
+            setUniform(m_transformationProjectionMatrix, matrix);
+            return *this;
+        }
+
+    } // namespace Examples
+} // namespace Magnum
--- src/TesselationShaderExample.cpp
+++ src/TesselationShaderExample.cpp
@@ -10,7 +10,9 @@
 #include <Magnum/Shaders/VertexColor.h>
 #include <Magnum/Math/Color.h>
 #include <Corrade/Containers/ArrayViewStl.h>
+
 #include "Line1DShaderProgram.h"
+#include "Quad2DShaderProgram.h"
 #include "Surface3DShaderProgram.h"
 
 #include <memory>
@@ -43,9 +45,9 @@ namespace Magnum
             SceneGraph::Camera3D* m_camera;
 
             Scene3D m_scene;
-            std::shared_ptr<SceneGraph::DrawableGroup3D> m_drawableGroup;
-            std::shared_ptr<Line1DShaderProgram> m_LineShader;
-            std::shared_ptr<Surface3DShaderProgram> m_SurfaceShader;
+            std::unique_ptr<Line1DShaderProgram> m_LineShader;
+            std::unique_ptr<Quad2DShaderProgram> m_QuadShader;
+            std::unique_ptr<Surface3DShaderProgram> m_SurfaceShader;
 
             GL::Buffer m_controlPointsBuffer{ NoCreate };
             GL::Mesh m_geometryMesh{ NoCreate }, m_controlPointsMesh;
@@ -68,8 +70,8 @@ namespace Magnum
 
 
             // initial vertices
-            set3DSurfaceTesselationControlPoints();
-
+            set2DQuadTesselationControlPoints();
+            Debug{}<<m_controlVertices;
             m_controlPointsShaderProgram = Shaders::VertexColor3D{};
             m_controlPointsShaderProgram.setTransformationProjectionMatrix(m_camera->projectionMatrix() * m_camera->cameraMatrix());
             GL::Renderer::setClearColor(Color4{ 0.0f, 0.1f, 0.4f, 1.0f });
@@ -80,7 +82,7 @@ namespace Magnum
         {
 
             GL::defaultFramebuffer.clear(GL::FramebufferClear::Color | GL::FramebufferClear::Depth);
-            renderSurface();
+            renderQuad();
             m_controlPointsShaderProgram.draw(m_controlPointsMesh);
             swapBuffers();
             redraw();
@@ -95,21 +97,31 @@ namespace Magnum
             m_controlPointsMesh = GL::Mesh(GL::MeshPrimitive::Points);
             m_controlPointsMesh.setCount(m_controlVertices.size())
                 .addVertexBuffer(m_controlPointsBuffer, 0, Shaders::VertexColor3D::Position{});
-            m_SurfaceShader->setLineWidth(1.0f)
-                .setLineColor(Color3{ .0f, 0.0f, 0.0f });
+
             GL::Renderer::setPatchVertexCount(m_controlVertices.size());
         }
         void TesselationShaderExample::set1DLineTesselationControlPoints() {
-            m_LineShader = std::make_shared<Line1DShaderProgram>();
+            m_LineShader = std::make_unique<Line1DShaderProgram>();
             m_controlVertices.clear();
             m_controlVertices.push_back({ -1.0f, -1.0f, 0.0f });
             m_controlVertices.push_back({ -1.0f, 1.0f, 0.0f });
             m_controlVertices.push_back({ 1.0f, 1.0f, 0.0f });
             setupMeshandBuffers();
         }
-        void TesselationShaderExample::set2DQuadTesselationControlPoints() {}
+        void TesselationShaderExample::set2DQuadTesselationControlPoints() {
+
+            m_QuadShader = std::make_unique<Quad2DShaderProgram>();
+
+            // initial vertices
+            m_controlVertices.clear();
+            m_controlVertices.push_back({ -1.0f, -1.0f, 0.0f });
+            m_controlVertices.push_back({ 1.0f, -1.0f, 0.0f });
+            m_controlVertices.push_back({ 1.0f, 1.0f, 0.0f });
+            m_controlVertices.push_back({ -1.0f, 1.0f, 0.0f });
+            setupMeshandBuffers();
+        }
         void TesselationShaderExample::set3DSurfaceTesselationControlPoints() {
-            m_SurfaceShader = std::make_shared<Surface3DShaderProgram>();
+            m_SurfaceShader = std::make_unique<Surface3DShaderProgram>();
             std::random_device rd;
             std::mt19937 mt(rd());
             std::uniform_real_distribution<double> dist(0.0, 1.0);
@@ -138,10 +150,21 @@ namespace Magnum
 
             m_LineShader->draw(m_geometryMesh);
         }
-        void TesselationShaderExample::renderQuad() {}
+        void TesselationShaderExample::renderQuad() {
+            m_QuadShader->setNumberOfInner(3)
+                .setNumberOfOuter(3)
+                .setLineWidth(1.5)
+                .setLineColor(Color3{ 1.0f, 0.0f, 0.0f })
+                .setQuadColor(Color3{ 1.0f, 1.0f, 1.0f })
+                .setViewportMatrix(200 * m_camera->projectionMatrix().inverted())
+                .setTransformationProjectionMatrix(m_camera->projectionMatrix() * m_camera->cameraMatrix());
+            m_QuadShader->draw(m_geometryMesh);
+        }
         void TesselationShaderExample::renderSurface() {
             m_cameraObject->rotateX(1.0_degf);
             m_SurfaceShader->setTessLevel(15)
+                .setLineWidth(1.0f)
+                .setLineColor(Color3{ .0f, 0.0f, 0.0f })
                 .setViewportMatrix(m_camera->projectionMatrix().inverted() * 200)
                 .setTransformationProjectionMatrix(m_camera->projectionMatrix() * m_camera->cameraMatrix());
             m_SurfaceShader->draw(m_geometryMesh);
